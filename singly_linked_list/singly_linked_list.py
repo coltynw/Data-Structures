61026a15adaf111ee79ba0c63fabf9affcbacf50
coltynw <coltynw@users.github.com>
2020-05-14 18:21:09 -0400

    pre sprint commit

---
+++ singly_linked_list/singly_linked_list.py
@@ -0,0 +1,62 @@
+class Node:
+  def __init__(self, value=None, next_node=None):
+    self.value = value
+    self.next_node = next_node
+
+  def get_value(self):
+    return self.value
+
+  def get_next(self):
+    return self.next_node
+
+  def set_next(self, new_next):
+    self.next_node = new_next
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def add_to_head(self, value):
+        new_node = Node(value)
+
+        if not self.head and not self.tail:
+            self.head = new_node
+            self.tail = new_node 
+        else:
+            new_node.set_next(self.head)
+            self.head = new_node
+
+    def add_to_tail(self, value):
+        new_node = Node(value)
+        if not self.head and not self.tail:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.set_next(new_node)
+            self.tail = new_node
+
+    def remove_head(self):
+        if not self.head:
+            return None
+        else:
+            value = self.head.get_value()
+            self.head = self.head.get_next()
+            return value
+
+    def print_ll_elements(self):
+        current = self.head
+
+        while current is not None:
+            print(current.value)
+            current = current.get_next()
+
+ll = LinkedList()
+ll.add_to_head(3)
+ll.add_to_head(5)
+ll.add_to_head(9)
+ll.add_to_head(11)
+ll.print_ll_elements()
+
+
+
